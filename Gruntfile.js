f2b531c95da5b8dc36d07dd107715b7c3b1b0122
Taka Kojima <taka@gigafied.com>
2013-12-09 22:25:30 -0800

    0.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,81 +1,69 @@
+/*
+ * grunt-tasks-lister
+ * https://github.com/gigafied/grunt-tasks-lister
+ *
+ * Copyright (c) 2013 Taka Kojima
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
 module.exports = function(grunt) {
 
-    grunt.initConfig({});
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>',
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
 
-    // Default task(s).
-    grunt.registerTask('default', ['tasks']);
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp'],
+    },
 
-    grunt.registerTask("tasks", "List all tasks", function () {
+    // Configuration to be run (and then tested).
+    tasks_lister: {
+      default_options: {
+        options: {
+        },
+        files: {
+          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
+        },
+      },
+      custom_options: {
+        options: {
+          separator: ': ',
+          punctuation: ' !!!',
+        },
+        files: {
+          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
+        },
+      },
+    },
 
-        var done = this.async();
-        var path = require('path');
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js'],
+    },
 
-        // Initialize task system so that the tasks can be listed.
-        grunt.task.init([], {help: true});
+  });
 
-        // Build 2-column array for table view.
-        var col1len = 0;
-        var opts = Object.keys(grunt.cli.optlist).map(function (long) {
-                var o = grunt.cli.optlist[long];
-                var col1 = '--' + (o.negate ? 'no-' : '') + long + (o.short ? ', -' + o.short : '');
-                col1len = Math.max(col1len, col1.length);
-                return [col1, o.info];
-        });
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
 
-        var taskNames = Object.keys(grunt.task._tasks).sort();
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-        var tasks = taskNames.map(function (name) {
-                var arr = [];
-                col1len = Math.max(col1len, name.length);
-                var info = grunt.task._tasks[name].info;
-                if (grunt.task._tasks[name].multi || name === "watch") {
-                        info += ' *';
-
-                        var config = grunt.config.get([name]);
-                        if (config) {
-                                arr.push(config);
-
-                                for (var key in config) {
-                                        col1len = Math.max(col1len, (name + ":" + key).length);
-                                }
-                        } else {
-                                arr.push("none");
-                        }
-                }
-                return [name, info].concat(arr);
-        });
-
-        // Widths for options/tasks table output.
-        var widths = [1, col1len, 2, 76 - col1len];
-
-        grunt.log.writeln();
-        tasks.forEach(function (a) {
-                var b = a[0].split(":"),
-                        c = a[1],
-                        opts = a[2];
-
-                if (b.length > 1) {
-                        b[0] = b[0].grey;
-                        b[1] = b[1].white;
-                }
-
-                b = b.join(":");
-
-                if (b !== "tasks" && (!opts || opts !== "none")) {
-                        grunt.log.writetableln(widths, ['', b.white, '', c.cyan]);
-
-                        if (opts) {
-                                for (var key in opts) {
-                                        if (key !== "" && opts.hasOwnProperty(key)) {
-                                                grunt.log.writetableln(widths, ['', b.grey + (":" + key).white, '', '']);
-                                        }
-                                }
-                        }
-                }
-        });
-        grunt.log.writeln();
-
-        done();
-    });
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'tasks']);
 
 };
